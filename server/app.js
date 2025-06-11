62714383abd8c01bf6f1d81648a56a7c50172103
samarth8804 <samarthmittal0808@gmail.com>
2025-06-11 18:54:53 +0530

    configured mongodb

--- server/app.js
+++ server/app.js
@@ -1,14 +1,22 @@
+const dotenv = require("dotenv");
+
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
+
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Welcome to car rental app");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("Error connecting to MongoDB : ", err);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+module.exports = connectDB;
+
+// This function connects to the MongoDB database using Mongoose.
+// It reads the connection string from the environment variable MONGO_URI.
