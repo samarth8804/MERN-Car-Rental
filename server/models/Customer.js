df41673914fa97002bc02e8164b8c51a5aba7bf9
samarth8804 <samarthmittal0808@gmail.com>
2025-06-11 18:55:11 +0530

    added customer model

---
+++ server/models/Customer.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const customerSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    phone: { type: String, required: true },
+    address: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", customerSchema);
