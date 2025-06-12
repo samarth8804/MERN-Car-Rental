1910d6429f16616b5a134e8512d39ce8a8a0ae8e
samarth8804 <samarthmittal0808@gmail.com>
2025-06-12 11:58:24 +0530

    created carOwner model

---
+++ server/models/CarOwner.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const carOwnerSchema = new mongoose.Schema(
+  {
+    fullname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true,match: /^[0-9]{10}$/ },
+    address: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("CarOwner", carOwnerSchema);
