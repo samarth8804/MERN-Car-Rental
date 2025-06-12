9128888a66c40b6cd9b05a9f76bc7a24b1d7bb9d
samarth8804 <samarthmittal0808@gmail.com>
2025-06-12 11:57:56 +0530

    created admin model

---
+++ server/models/Admin.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const adminSchema = new mongoose.Schema(
+  {
+    fullname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true, match: /^[0-9]{10}$/ },
+    address: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Admin", adminSchema);
+ 
