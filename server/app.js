f0c8ef48cb137b5b68e94514dc733a2002fbc03a
samarth8804 <samarthmittal0808@gmail.com>
2025-06-13 23:50:26 +0530

    added routes

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 
@@ -10,11 +11,34 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Import routes
+const adminRoutes = require("./routes/adminRoutes");
+const carOwnerRoutes = require("./routes/carOwnerRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+const authRoutes = require("./routes/authRoutes");
+const customerRoutes = require("./routes/customerRoutes");
+const carRoutes = require("./routes/carRoutes");
+const driverRoutes = require("./routes/driverRoutes");
+
+// Middleware to handle CORS
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Adjust this to your frontend URL
+    methods: ["GET,POST,PUT,DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Welcome to car rental app");
-});
+// routes
+app.use("/api/v1/admin", adminRoutes);
+app.use("/api/v1/car-owner", carOwnerRoutes);
+app.use("/api/v1/booking", bookingRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/customer", customerRoutes);
+app.use("/api/v1/car", carRoutes);
+app.use("/api/v1/driver", driverRoutes);
 
 const PORT = process.env.PORT || 5000;
 
