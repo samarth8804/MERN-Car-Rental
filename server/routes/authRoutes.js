0ee5ff7d94f142d55a3761fa14ea889173c842d3
samarth8804 <samarthmittal0808@gmail.com>
2025-06-14 17:38:51 +0530

    added register,create and login routes for all users

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const {
+  loginAdmin,
+  registerCustomer,
+  loginCustomer,
+  registerCarOwner,
+  loginCarOwner,
+  registerDriver,
+  loginDriver,
+  createCustomer,
+  createCarOwner,
+  createDriver,
+} = require("../controllers/authController");
+
+// Admin login route
+router.post("/admin/login", loginAdmin);
+
+// Customer registration and login routes
+router.post("/register/customer", registerCustomer);
+router.post("/create/customer", createCustomer);
+router.post("/login/customer", loginCustomer);
+
+// Car Owner registration and login routes
+
+router.post("/register/car-owner", registerCarOwner);
+router.post("/create/car-owner", createCarOwner);
+router.post("/login/car-owner", loginCarOwner);
+
+// Driver registration and login routes
+
+router.post("/register/driver", registerDriver);
+router.post("/create/driver", createDriver);
+router.post("/login/driver", loginDriver);
+
+module.exports = router;
