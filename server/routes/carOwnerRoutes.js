8f935d897498983aff2dca2bcf2ec07493eb8ba4
samarth8804 <samarthmittal0808@gmail.com>
2025-06-18 00:49:43 +0530

    added carOwner routes

--- server/routes/carOwnerRoutes.js
+++ server/routes/carOwnerRoutes.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const {
+  addCar,
+  deleteCar,
+  updateCar,
+  getMyCars,
+} = require("../controllers/carOwnerController");
+const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Welcome to the car rental app");
-});
+// Car Owner Adds a Car
+router.post("/add-car", protect(["carOwner"]), addCar);
+
+// Car Owner gets own cars
+router.get("/my-cars", protect(["carOwner"]), getMyCars);
+
+// Car Owner updates a Car
+router.put("/update-car/:id", protect(["carOwner"]), updateCar);
+
+// Car Owner deletes a Car
+router.delete("/delete-car/:id", protect(["carOwner"]), deleteCar);
 
 module.exports = router;
