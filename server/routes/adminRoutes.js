cc65e3b57e3b461d95027a0cce74a47a3a24e0b5
samarth8804 <samarthmittal0808@gmail.com>
2025-06-18 18:44:01 +0530

    added admin routes

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,8 +1,34 @@
 const express = require("express");
+const {
+  getAllCars,
+  approveCar,
+  rejectCar,
+  deleteCar,
+  getAllDrivers,
+  approveDriver,
+  rejectDriver,
+  deleteDriver,
+  getAllBookings,
+} = require("../controllers/adminController");
 const router = express.Router();
+const { protect } = require("../middlewares/authMiddleware");
 
-router.get("/", (req, res) => {
-  res.send("Welcome to the car rental app");
-});
+router.get("/getAllCars", protect(["admin"]), getAllCars);
+
+router.patch("/approveCar", protect(["admin"]), approveCar);
+
+router.patch("/rejectCar", protect(["admin"]), rejectCar);
+
+router.delete("/deleteCar/:carId", protect(["admin"]), deleteCar);
+
+router.get("/getAllDrivers", protect(["admin"]), getAllDrivers);
+
+router.patch("/approveDriver", protect(["admin"]), approveDriver);
+
+router.patch("/rejectDriver", protect(["admin"]), rejectDriver);
+
+router.delete("/deleteDriver/:driverId", protect(["admin"]), deleteDriver);
+
+router.get("/getAllBookings", protect(["admin"]), getAllBookings);
 
 module.exports = router;
