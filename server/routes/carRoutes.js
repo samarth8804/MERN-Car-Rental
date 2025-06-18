064ad0f2cd4755dbdcb2c00dd143750ca392a101
samarth8804 <samarthmittal0808@gmail.com>
2025-06-18 18:44:27 +0530

    added car-details route

--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const { getCarDetails } = require("../controllers/carController");
+const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.get("/car-details/:id", getCarDetails);
+router.get(
+  "/car-details/:id",
+  protect(["admin", "customer", "driver", "carOwner"]),
+  getCarDetails
+);
 
 module.exports = router;
