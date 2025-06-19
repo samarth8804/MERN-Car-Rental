90a752fd3ff9f3ecd4645aacdd0bbd7bba2792be
samarth8804 <samarthmittal0808@gmail.com>
2025-06-19 22:34:44 +0530

    added send notification via email

---
+++ server/utils/sendNotifications.js
@@ -0,0 +1,18 @@
+const nodemailer = require("nodemailer");
+
+exports.sendEmail = async (to, subject, html) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"Car Rental App" <${process.env.EMAIL_USER}`,
+    to,
+    subject,
+    html,
+  });
+};
