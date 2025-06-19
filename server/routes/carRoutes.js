847d81a44aef1e5fa804e4b0e993dcdee36932f2
samarth8804 <samarthmittal0808@gmail.com>
2025-06-19 22:34:07 +0530

    added delete car route

--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getCarDetails } = require("../controllers/carController");
+const { getCarDetails, deleteCar } = require("../controllers/carController");
 const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
@@ -8,5 +8,8 @@ router.get(
   protect(["admin", "customer", "driver", "carOwner"]),
   getCarDetails
 );
+
+// Car Owner or admin deletes a Car
+router.delete("/delete-car/:carId", protect(["carOwner", "admin"]), deleteCar);
 
 module.exports = router;
