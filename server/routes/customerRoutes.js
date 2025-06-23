1f78ba02f34c64c50afccdf830b7f63f42461708
samarth8804 <samarthmittal0808@gmail.com>
2025-06-23 19:51:22 +0530

    added all customer routes

--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const {
+  getAvailableCars,
+  bookCar,
+  cancelBooking,
+  getCustomerProfile,
+  updateCustomerProfile,
+} = require("../controllers/customerController");
 const router = express.Router();
+const { protect } = require("../middlewares/authMiddleware");
 
-router.get("/", (req, res) => {
-  res.send("Welcome to the car rental app");
-});
+router.get("/get-available-cars", protect(["customer"]), getAvailableCars);
+
+router.post("/book-car", protect(["customer"]), bookCar);
+
+router.post("/cancel-booking", protect(["customer"]), cancelBooking);
+
+router.get("/get-customer-profile", protect(["customer"]), getCustomerProfile);
+
+router.put(
+  "/update-customer-profile",
+  protect(["customer"]),
+  updateCustomerProfile
+);
 
 module.exports = router;
