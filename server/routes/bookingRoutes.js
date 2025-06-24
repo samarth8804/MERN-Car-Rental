d48b69494bc04bf3d26416b1fffb6e3eb1d52bb5
samarth8804 <samarthmittal0808@gmail.com>
2025-06-25 00:38:07 +0530

    added route for booking-history and single-booking

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -1,8 +1,24 @@
 const express = require("express");
-const { cancelBooking } = require("../controllers/bookingsController");
+const {
+  cancelBooking,
+  getBookingHistory,
+  getSingleBooking,
+} = require("../controllers/bookingsController");
 const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.post("/cancel-booking", protect(["customer","admin"]), cancelBooking);
+router.get(
+  "/get-booking-history",
+  protect(["customer", "admin", "driver", "carOwner"]),
+  getBookingHistory
+);
+
+router.get(
+  "/get-single-booking/:bookingId",
+  protect(["customer", "admin", "driver", "carOwner"]),
+  getSingleBooking
+);
+
+router.post("/cancel-booking", protect(["customer", "admin"]), cancelBooking);
 
 module.exports = router;
