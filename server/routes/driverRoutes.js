097de32fabd0f93505205dcddbd47d4d9562a9b4
samarth8804 <samarthmittal0808@gmail.com>
2025-07-04 12:23:37 +0530

    added end-ride and complete-ride routes

--- server/routes/driverRoutes.js
+++ server/routes/driverRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { deleteDriver } = require("../controllers/driverController");
+const {
+  deleteDriver,
+  endRide,
+  completeRide,
+} = require("../controllers/driverController");
 const { protect } = require("../middlewares/authMiddleware");
 
 router.delete(
@@ -8,5 +12,9 @@ router.delete(
   protect(["admin", "driver"]),
   deleteDriver
 );
+
+router.post("/end-ride", protect(["driver"]), endRide);
+
+router.post("/complete-ride", protect(["driver"]), completeRide);
 
 module.exports = router;
