d66d1693f48f1000448a978997f87ca47b783755
samarth8804 <samarthmittal0808@gmail.com>
2025-08-31 15:04:30 +0530

    feat: add routes to fetch all customers and car owners with admin protection

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -6,6 +6,8 @@ const {
   getAllDrivers,
   approveDriver,
   rejectDriver,
+  getAllCustomers,
+  getAllCarOwners
 } = require("../controllers/adminController");
 const router = express.Router();
 const { protect } = require("../middlewares/authMiddleware");
@@ -22,5 +24,8 @@ router.patch("/approveDriver/:driverId", protect(["admin"]), approveDriver);
 
 router.patch("/rejectDriver/:driverId", protect(["admin"]), rejectDriver);
 
+router.get("/getAllCustomers", protect(["admin"]), getAllCustomers);
+
+router.get("/getAllCarOwners", protect(["admin"]), getAllCarOwners);
 
 module.exports = router;
