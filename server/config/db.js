1437be06abe5cc8c58b0df60112dd3ff4795c676
samarth8804 <samarthmittal0808@gmail.com>
2026-01-20 17:16:23 +0530

    fix: comment out process.exit in MongoDB connection error handling

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("Error connecting to MongoDB : ", err);
-    process.exit(1); // Exit the process with failure
+    // process.exit(1); // Exit the process with failure
   }
 };
 
